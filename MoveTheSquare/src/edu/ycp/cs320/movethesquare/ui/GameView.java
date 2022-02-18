736e8ebe1405a8fafd46200d162b7b8aec652ff1
amott <amott@10.127.4.122>
2022-02-18 02:39:27 -0500

    Changed square to circle, lowered size to 30 pixels x and y, changed color of circle and background.

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -18,6 +18,7 @@ import edu.ycp.cs320.movethesquare.model.Square;
 
 public class GameView extends JPanel {
 	private static final Color MIDNIGHT_BLUE = new Color(25, 25, 112);
+	private static final Color GRAY = new Color(211, 211, 211);
 	
 	private Game model;
 	private GameController controller;
@@ -26,7 +27,7 @@ public class GameView extends JPanel {
 	public GameView(Game model) {
 		this.model = model;
 		setPreferredSize(new Dimension((int) model.getWidth(), (int)model.getHeight()));
-		setBackground(MIDNIGHT_BLUE);
+		setBackground(GRAY);
 
 		// djh2-KEC119-21: changed from 30 to 45
 		// djh2-YCPlaptop: change from 45 to 100
@@ -66,11 +67,12 @@ public class GameView extends JPanel {
 		
 		// djh2-KEC110-21: changed from GREEN to RED
 		// djh2-YCPlaptop: change from RED to YELLOW
-		g.setColor(Color.YELLOW);
+		// amott-PC: change from YELLOW to BLUE
+		g.setColor(Color.BLUE);
 
 		Square square = model.getSquare();
 		
-		g.fillRect((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
+		g.fillOval((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
 	}
 	
 	public static void main(String[] args) {
@@ -84,8 +86,8 @@ public class GameView extends JPanel {
 				Square square = new Square();
 				square.setX(300.0);
 				square.setY(220.0);
-				square.setWidth(40.0);
-				square.setHeight(40.0);
+				square.setWidth(30.0);
+				square.setHeight(30.0);
 				model.setSquare(square);
 				
 				GameController controller = new GameController();
@@ -93,7 +95,7 @@ public class GameView extends JPanel {
 				GameView view = new GameView(model);
 				view.setController(controller);
 				
-				JFrame frame = new JFrame("Move the Square!");
+				JFrame frame = new JFrame("Move the Circle!");
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.add(view);
 				frame.pack();
